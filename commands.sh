d9f5d27a8334bda7fa0b4620c51f4106d6416b65
Agustin Andres Gutierrez <108552727+AgustinGuti@users.noreply.github.com>
2024-05-29 20:33:34 +0000

    Add mongo setup commands

--- commands.sh
+++ commands.sh
@@ -1,4 +1,12 @@
+docker pull mongo
+docker run --name Mymongo -p 27017:27017 -d mongo
+
 docker cp albumlist.csv Mymongo:/albumlist.csv;
 docker exec -it Mymongo bash;
-mongoimport -d local --collection albums --type csv --headerline < albumlist.csv;
 
+# From inside the docker
+mongoimport -d local --collection albums --type csv --headerline < albumlist.csv;
+mongosh
+
+# From inside mongosh
+use local;
